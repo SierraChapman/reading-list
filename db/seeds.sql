1a62e449f1e5394998b15d14be76f11c13f3a565
Sierra Chapman <siechap@gmail.com>
2020-07-23 21:28:39 -0700

    write SQL to seed the database with example values

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- for initializing database with some data
+
+INSERT INTO books (title, was_read) VALUES ("Mistborn", TRUE);
+INSERT INTO books (title, was_read) VALUES ("The Well of Ascension", TRUE);
+INSERT INTO books (title) VALUES ("The Hero of Ages");
