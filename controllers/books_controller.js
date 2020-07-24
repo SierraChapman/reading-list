a482be9adf7f1e620ba1f1be3b54df05f0d4e7fd
Sierra Chapman <siechap@gmail.com>
2020-07-23 20:25:58 -0700

    create simple router

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,1 +1,12 @@
 // for creating routes
+
+const express = require("express");
+
+const router = express.Router();
+
+// super simple route for testing setup
+router.get("/", (req, res) => {
+  res.render("index", {title: "Reading List"});
+});
+
+module.exports = router;
