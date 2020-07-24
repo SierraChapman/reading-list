6adf010e149ca5dcc873696b41c12ed369d792a4
Sierra Chapman <siechap@gmail.com>
2020-07-23 21:55:51 -0700

    create getAll method for books model

--- models/books.js
+++ models/books.js
@@ -1,1 +1,14 @@
 // for applying the orm to the needs of the reading list
+
+const orm = require("../config/orm.js");
+
+const books = {
+  getAll: function(cb) {
+    // gets data for all the books
+    orm.selectAll("books", cb);
+  },
+  // add -- adds a new book
+  // markRead -- updates was_read to true for a book
+};
+
+module.exports = books;
