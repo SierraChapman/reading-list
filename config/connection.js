39e9818eb7f8e7613b5121e8601e475129235112
Sierra Chapman <siechap@gmail.com>
2020-07-23 21:35:31 -0700

    create connection to database

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,21 @@
 // for connecting to database
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "reading_list_db"
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error("Database connection error: " + err.stack);
+    return;
+  }
+  console.log("Database connected with id " + connection.threadId);
+});
+
+module.exports = connection;
