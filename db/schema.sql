8af815d79feb3cdec607f768943dfc38f9e68c79
Sierra Chapman <siechap@gmail.com>
2020-07-23 21:23:45 -0700

    make schema for creating database

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
 -- for creating database
+
+CREATE DATABASE reading_list_db;
+USE reading_list_db;
+
+CREATE TABLE books(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    was_read BOOLEAN DEFAULT FALSE
+);
