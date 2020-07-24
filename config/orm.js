1ee6b3905f7e6bcc06825f6d94e9863f04471a4c
Sierra Chapman <siechap@gmail.com>
2020-07-23 21:45:16 -0700

    create selectAll function for ORM

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,15 @@
 // for interacting with database
+
+const connection = require("./connection.js");
+
+const orm = {
+  selectAll: function(tableName, cb) {
+    // gets all data in the table identified by tableName and calls cb on the result
+    connection.query("SELECT * FROM ??", tableName, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+};
+
+module.exports = orm;
