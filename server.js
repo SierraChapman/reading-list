13fc9429c9e4611482544dfe103ec0a7789d3eb9
Sierra Chapman <siechap@gmail.com>
2020-07-23 20:36:29 -0700

    create simple server

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // for creating the server
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const routes = require("./controllers/books_controller.js");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("App now listening at localhost:" + PORT);
+});
