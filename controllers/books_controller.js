c712d77132d6bb2d7942f5d767d2c08c1d951eea
Sierra Chapman <siechap@gmail.com>
2020-07-25 13:33:13 -0700

    create api route for adding a book

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -6,10 +6,23 @@ const books = require("../models/books.js");
 
 const router = express.Router();
 
-// super simple route for testing setup
+// home page
 router.get("/", (req, res) => {
   books.getAll(data => {
     res.render("index", {books: data});
+  });
+});
+
+// adding a book
+router.post("/api/books/new", (req, res) => {
+  books.add(req.body.title, result => {
+    if (result.affectedRows === 0) {
+      // No rows were affected, so there must have been an error.
+      return res.status(500).end();
+    } else {
+      // Rows were affected. Success!
+      res.status(200).end();
+    }
   });
 });
 
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 // for creating the server
 
+// import dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
 
 const routes = require("./controllers/books_controller.js");
 
+// create the app
 const app = express();
-
 const PORT = process.env.PORT || 8080;
 
+// parse data sent in requests
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// use handlebars templates
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// add routes
 app.use(routes);
 
+// listen
 app.listen(PORT, () => {
   console.log("App now listening at localhost:" + PORT);
 });
